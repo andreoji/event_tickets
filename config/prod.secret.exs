8c285aadf0d231b8e9f36b6ce0c7d5f426005c8e
andre oji <andre.oji@trx.tv>
2019-12-31 14:50:32 +0000

    Add Heroku prod config

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,7 +12,7 @@ database_url =
     """
 
 config :naive_dice, NaiveDice.Repo,
-  # ssl: true,
+  ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
